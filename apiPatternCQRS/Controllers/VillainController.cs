68986dd3e97270073356b9917fe7cec76dde7459
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-26 15:42:25 -0300

    Implementando controller e configurando mediatR

--- apiPatternCQRS/Controllers/VillainController.cs
+++ apiPatternCQRS/Controllers/VillainController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apiPatternCQRS.WebApi.Application.Models.RequestModels.CommandRequestModels;
+using apiPatternCQRS.WebApi.Application.Models.RequestModels.QueryRequestModels;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,16 +13,42 @@ namespace apiPatternCQRS.Controllers
     [ApiController]
     [Route("[controller]")]
     public class VillainController : ControllerBase
-    {       
+    {
+        private readonly IMediator _mediator;
 
-        public VillainController()
+        public VillainController(IMediator mediator)
         {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public IActionResult CreateVillain([FromBody] InsertVillainRequestModel request)
+        {
+            try
+            {
+                var response = _mediator.Send(request);
+                return Created(string.Empty, response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetVillainById([FromBody] GetVillainByIdRequestModel request)
         {
-            return Ok();
+            try
+            {
+                var response = _mediator.Send(request);
+                return Created(string.Empty, response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
     }
 }
--- apiPatternCQRS/Startup.cs
+++ apiPatternCQRS/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MediatR;
+using apiPatternCQRS.WebApi.Repositories.Interfaces;
+using apiPatternCQRS.WebApi.Repositories.Implementations;
 
 namespace apiPatternCQRS
 {
@@ -26,6 +30,8 @@ namespace apiPatternCQRS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMediatR(typeof(Startup));
+            services.AddSingleton<IVillainRepository, VillainRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
